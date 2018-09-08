17a93b42270224bc986069f1a3e0fa9486b04a9a
jerrry1232 <gerardo.garcia.q@gmail.com>
2018-09-08 10:00:00 -0500

    Se agrega archivos de gerardo

---
+++ src/gerardogarcia/Constructores/TestBanco.java
@@ -0,0 +1,10 @@
+package gerardogarcia.Constructores;
+
+public class TestBanco {
+
+	public static void main (String [] args) {
+		CuentaBancaria cuentafabi = new CuentaBancaria();
+		
+		CuentaBancaria cuentaleo = new CuentaBancaria("lenardo");
+	}
+}
